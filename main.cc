3e0635c8f3588403c41285dcb02ec5785c64a9bf
Alex Trajano <aferreira91@gmail.com>
2018-11-15 10:10:44 -0300

    First simulation

---
+++ main.cc
@@ -0,0 +1,19 @@
+#include "ns3/core-module.h"
+#include "playground.cc"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("SmartGridSimulation");
+
+int main(int argc, char *argv[])
+{
+  playground_enable_logging();
+  playground_build_topology();
+
+  NS_LOG_INFO("Run Simulation.");
+  Simulator::Run();
+  Simulator::Destroy();
+  NS_LOG_INFO("Done.");
+
+  return 0;
+}
