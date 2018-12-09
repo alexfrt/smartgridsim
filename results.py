b6b4e0bb1e89ffba78d55c44b5e5c91d9e067a39
Alex Trajano <aferreira91@gmail.com>
2018-12-09 10:11:38 -0300

    making plots and statistics summary of ns3 outputs

--- results.py
+++ results.py
@@ -2,20 +2,44 @@
 
 import xmltodict
 import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import describe
 
-with open('outputs/FlowMon.xml') as fd:
-    doc = xmltodict.parse(fd.read())
 
-flows = doc['FlowMonitor']['FlowStats']['Flow']
+def main():
+    with open('outputs/FlowMon.xml') as fd:
+        doc = xmltodict.parse(fd.read())
 
-lostPercentage = list()
-delayAverages = list()
+    flows = doc['FlowMonitor']['FlowStats']['Flow']
 
-for flow in flows:
-    lostPercentage.append(int(flow[u'@lostPackets']) / int(flow[u'@txPackets']))
-    rxPackets = int(flow[u'@rxPackets'])
-    if rxPackets > 0:
-        delayAverages.append(float(flow[u'@delaySum'][1:-2]) / 1e-6 / int(flow[u'@rxPackets']))
-    # flow[u'@jitterSum']
+    lost_percentages = list()
+    delay_averages = list()
+    jitter_averages = list()
 
-sns.distplot(delayAverages)
+    for flow in flows:
+        lost_percentages.append(100.0 * float(flow[u'@lostPackets']) / float(flow[u'@txPackets']))
+        if int(flow[u'@rxPackets']) > 1:
+            delay_averages.append(float(flow[u'@delaySum'][1:-2]) / 1e6 / int(flow[u'@rxPackets']))
+            jitter_averages.append(float(flow[u'@jitterSum'][1:-2]) / 1e6 / int(flow[u'@rxPackets']))
+
+    make_figure_and_print_summary(lost_percentages, 'lostPercentages')
+    make_figure_and_print_summary(delay_averages, 'delayAverages')
+    make_figure_and_print_summary(jitter_averages, 'jitterAverages')
+
+
+def make_figure_and_print_summary(series, name):
+    sns.distplot(series)
+    plt.savefig('outputs/%s.svg' % name)
+    plt.show()
+
+    stats = describe(series)
+    mean = getattr(stats, 'mean')
+    variance = getattr(stats, 'variance')
+    std_deviation = np.sqrt(variance)
+
+    print("{:<15} - Mean: {:<6.2f} - StdDev: {:<6.2f} - Variance: {:<8.2f}".format(name, mean, std_deviation, variance))
+
+
+if __name__ == '__main__':
+    main()
